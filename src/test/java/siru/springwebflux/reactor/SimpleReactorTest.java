8cde40c07321a370f65758ea023a96a96c684a05
shinwoo <sinwoo1225@gmail.com>
2021-11-23 19:18:27 +0900

    reactor 예제추가
    - stepverifier
    - interval
    - webclient 수정
    - mergeWith, zip

--- src/test/java/siru/springwebflux/reactor/SimpleReactorTest.java
+++ src/test/java/siru/springwebflux/reactor/SimpleReactorTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,10 +36,10 @@ public class SimpleReactorTest {
     public void test_iterable_flux() throws Exception {
         List<String> singerNameList = new ArrayList<>();
 
-       Flux.fromIterable(Arrays.asList("아이유", "이무진"))
-               .subscribe(singerNameList::add);
+        Flux.fromIterable(Arrays.asList("아이유", "이무진"))
+                .subscribe(singerNameList::add);
 
-       assertThat(singerNameList).asList().isEqualTo(Arrays.asList("아이유", "이무진"));
+        assertThat(singerNameList).asList().isEqualTo(Arrays.asList("아이유", "이무진"));
     }
 
     @Test
@@ -50,6 +53,82 @@ public class SimpleReactorTest {
     }
 
     @Test
+    public void stepVerifier_test() throws Exception {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        Flux<Integer> flux = Flux.fromIterable(list)
+                .log();
+
+        StepVerifier.create(flux)
+                .expectNext(1,2,3,4)
+                .verifyComplete();
+
+        StepVerifier.create(flux)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void interval_test() throws Exception {
+        Flux<Long> intervalFlux =
+                Flux.interval(Duration.ofSeconds(1))
+                        .log()
+                        .take(10); // 1초 간격으로 1씩 증가하면서 정수데이터 방출 (10개 받을때까지)
+
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L, 1L ,2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mergeFluxes_test() throws Exception {
+        // given
+        Flux<String> characterFlux =
+                Flux.just("SpongeBob", "Patrick", "Squidward")
+                        .delayElements(Duration.ofMillis(500));
+        Flux<String> foodFlux =
+                Flux.just("Apples", "Lollipops")
+                        .delaySubscription(Duration.ofMillis(250))
+                        .delayElements(Duration.ofMillis(500));
+
+        //when
+        Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux).log();
+
+        // then
+        StepVerifier.create(mergedFlux)
+                .expectNext("SpongeBob", "Apples", "Patrick", "Lollipops", "Squidward")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zip_test() throws Exception {
+        // given
+        Flux<String> characterFlux =
+                Flux.just("SpongeBob", "Patrick", "Squidward");
+        Flux<String> foodFlux =
+                Flux.just("Apples", "Lollipops", "Lasagna");
+
+        //when
+        Flux<Tuple2<String, String>> zippedFlux = Flux.zip(characterFlux, foodFlux).log();
+
+        // then
+        StepVerifier.create(zippedFlux)
+                .expectNextMatches(t ->
+                    t.getT1().equals("SpongeBob") &&
+                    t.getT2().equals("Apples"))
+                .expectNextMatches(t ->
+                        t.getT1().equals("Patrick") &&
+                        t.getT2().equals("Lollipops"))
+                .expectNextMatches(t ->
+                        t.getT1().equals("Squidward") &&
+                        t.getT2().equals("Lasagna"))
+                .verifyComplete();
+    }
+
+    @Test
     public void flux_lazy_test() throws Exception {
 
         Flux<Long> flux = Flux.range(1,9)
@@ -57,14 +136,12 @@ public class SimpleReactorTest {
                     try{
                         Thread.sleep(1000);
                     } catch (InterruptedException ignored) {}
-                    System.out.println("호옹");
+                    System.out.println("호옹") ;
                     return Mono.just(3L * n);
                 });
 
         System.out.println("구독전...");
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException ignored) {}
+
         flux.subscribe(value -> {
                     System.out.println("value: " + value);
                 },
@@ -96,33 +173,19 @@ public class SimpleReactorTest {
     @Test
     public void webClient_test() throws Exception {
         WebClient webClient = WebClient.create();
-        Mono<String> resultMono = webClient.get()
-                .uri("http://localhost:8080/test")
-                .retrieve()
-                .bodyToMono(String.class);
-
-        String result = resultMono.block();
-
-        assertThat(result).isEqualTo("test is tasty");
-    }
-
-    @Test
-    public void webClient_toFlux_test() throws Exception {
-        WebClient webClient = WebClient.create();
-        webClient.get()
+        List<String> result = webClient.get()
                 .uri("http://localhost:8080/users")
                 .retrieve()
-                .bodyToMono(List.class)
+                .bodyToFlux(Integer.class)
+                .flatMap(ui -> webClient
+                        .get()
+                        .uri("http://localhost:8080/users/{userIndex}", ui)
+                        .retrieve()
+                        .bodyToMono(String.class))
                 .log()
-                        .flatMapMany(Flux::fromIterable)
-                                .flatMap(userIndex ->
-                                    webClient.get()
-                                            .uri("http://localhost:8080/users/{userIndex}", userIndex)
-                                            .retrieve()
-                                            .bodyToMono(String.class)
-                                )
-                                        .subscribe(System.out::println);
-        Thread.sleep(10000);
-        System.out.println("데이터 전송완료");
+                .collectList()
+                .block();
+        System.out.println(result);
+        assertThat(result.size()).isEqualTo(3);
     }
 }
